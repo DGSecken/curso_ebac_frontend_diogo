f2add57e034f128e5abdb22342d11865c58f624b
Diogo <diogosella@outlook.com>
2023-10-17 19:27:55 -0300

    homework

---
+++ homework.js
@@ -0,0 +1,5 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(multiplicacao(20, 10));
---
+++ homework.ts
@@ -0,0 +1,10 @@
+function multiplicacao(numero1: number, numero2: number) {
+    return numero1 * numero2
+}
+
+function dizOi(nome: string) {
+    return nome + " te disse oi :D"
+}
+
+console.log(multiplicacao(20,10))
+console.log(dizOi("Mariana"))
