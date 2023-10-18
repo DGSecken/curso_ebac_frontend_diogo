8a552a4841ce446eb2d17a93bc3bec7e229af1a0
Diogo <diogosella@outlook.com>
2023-10-17 22:39:11 -0300

    fixed homework

--- homework.ts
+++ homework.ts
@@ -1,8 +1,8 @@
-function multiplicacao(numero1: number, numero2: number) {
+function multiplicacao(numero1: number, numero2: number): number {
     return numero1 * numero2
 }
 
-function dizOi(nome: string) {
+function dizOi(nome: string): string {
     return nome + " te disse oi :D"
 }
 
